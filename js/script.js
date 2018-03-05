a2e363825e558da08b8d6daa4271fbe4b6a4f853
Rafal Swieton <rafal@eskrzynka.pl>
2018-03-05 22:17:11 +0100

    Change files

--- js/script.js
+++ js/script.js
@@ -2,14 +2,50 @@
 
 $(function() {
 	var carouselList = $("#carousel ul"); 
+	var nextImage = $("#carousel .next");
+	var prevImage = $("#carousel .prev");
+	
+	function changeImage() {
+		carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
+		function moveFirstSlide () {
+			var firstItem = carouselList.find("li:first");
+			var lastItem =carouselList.find("li:last");
+			lastItem.after(firstItem);
+			carouselList.css({marginLeft:0});
+		}
+	} 
+	setInterval(changeImage, 3000);
 
-function changeImage() {
-	carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
-	function moveFirstSlide () {
+	nextImage.click(changeImage);
+
+	function moveLastSlide() {
 		var firstItem = carouselList.find("li:first");
 		var lastItem =carouselList.find("li:last");
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft:-400});
+		carouselList.animate({marginLeft: 0}, 500);
 	}
-} setInterval(changeImage, 3000);
+	
+
+	prevImage.click(moveLastSlide);
+
+	/*var interval;
+  
+  	interval = setInterval(function() {
+
+  	}, 3000);
+  
+  	clearInterval(interval);
+
+  	carouselList.hover(function() {
+      	clearInterval(interval);
+    },
+    	function() {
+    		interval = setInterval(function() {
+
+    		}, 3000);
+    	}
+    }
+  );*/
+  	
 });
